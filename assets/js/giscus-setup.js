300e4cfdc300931e970b0e0754be48bae448dc41
imsanika03 <imsanika03@gmail.com>
2026-08-10 08:58:03 +0000

    Deploying to gh-pages from @ imsanika03/imsanika03.github.io@9b41011c00864dbde248d7d664926d0e20895f9c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "imsanika03/sanika-web",
+    "data-repo": "imsanika03/imsanika03.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
